ded68d332238c59ca499535e691de7f0a5fb34c8
Noah <noahdahms@gmail.com>
2023-11-27 23:05:18 +0100

    fixes, added config and rules

--- keyboards/beekeeb/piantor_pro/keymaps/SwaggyPinguin/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/SwaggyPinguin/keymap.c
@@ -7,8 +7,8 @@ enum keyboard_layers {
   _BL = 0, // Base Layer
   _SL,     // Symbols Layer / Number Layer
   _FL,     // Function Layer / Navigation Layer
-  _ML      // Media Layer
-  _GL      // Gaming Layer
+  _ML,     // Media Layer
+  _GL,     // Gaming Layer
   _CM,     // Colemak Layer
 };
 
